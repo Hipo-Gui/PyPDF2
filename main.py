9fbb862e4f2472cd46ba03ba5af93221547731d9
Hipo-Gui <luizguilhermehipolito2@gmail.com>
2026-01-08 10:55:26 -0300

    Pdf Merger

---
+++ main.py
@@ -0,0 +1,48 @@
+# # PyPDF2 para manipular arquivos PDF (Instalação)
+# PyPDF2 é uma biblioteca de manipulação de arquivos PDF feita em Python puro,
+# gratuita e de código aberto. Ela é capaz de ler, manipular, escrever e unir
+# dados de arquivos PDF, assim como adicionar anotações, transformar páginas,
+# extrair texto e imagens, manipular metadados, e mais.
+# A documentação contém todas as informações necessárias para usar PyPDF2.
+# Link: https://pypdf2.readthedocs.io/en/3.0.0/
+# Ative seu ambiente virtual
+# pip install pypdf2
+
+
+# PyPDF2 para manipular arquivos PDF (PdfWriter)
+# PyPDF2 para manipular arquivos PDF (PdfMerger)
+# PyPDF2 é uma biblioteca de manipulação de arquivos PDF feita em Python puro,
+# gratuita e de código aberto. Ela é capaz de ler, manipular, escrever e unir
+# dados de arquivos PDF, assim como adicionar anotações, transformar páginas,
+
+
+
+
+"""
+from pathlib import Path
+
+
+from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2 import PdfMerger, PdfReader, PdfWriter
+
+PASTA_RAIZ = Path(__file__).parent
+PASTA_ORIGINAIS = PASTA_RAIZ / 'pdfs_originais'
+@@ -39,3 +39,17 @@
+    with open(PASTA_NOVA / f'page{i}.pdf', 'wb') as arquivo:
+        writer.add_page(page)
+        writer.write(arquivo)  # type: ignore
+
+
+files = [
+    PASTA_NOVA / 'page1.pdf',
+    PASTA_NOVA / 'page0.pdf',
+
+]
+
+merger = PdfMerger()
+for file in files:
+    merger.append(file)  # type: ignore
+
+merger.write(PASTA_NOVA / 'MERGED.pdf')  # type: ignore
+merger.close()
+"""
